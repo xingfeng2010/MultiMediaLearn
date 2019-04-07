cae31c8ecd233ac84bc6c4aabfe15a4505feeb4d
xingfeng2010 <583354000@qq.com>
2019-04-07 20:03:19 +0800

    合成一个ffmpeg库

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -2,43 +2,8 @@ LOCAL_PATH := $(call my-dir)
 
 # FFmpeg library
 include $(CLEAR_VARS)
-LOCAL_MODULE := avcodec
-LOCAL_SRC_FILES := libavcodec-57.so
-include $(PREBUILT_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := avdevice
-LOCAL_SRC_FILES := libavdevice-57.so
-include $(PREBUILT_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := avfilter
-LOCAL_SRC_FILES := libavfilter-6.so
-include $(PREBUILT_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := avformat
-LOCAL_SRC_FILES := libavformat-57.so
-include $(PREBUILT_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := avutil
-LOCAL_SRC_FILES := libavutil-55.so
-include $(PREBUILT_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := postproc
-LOCAL_SRC_FILES := libpostproc-54.so
-include $(PREBUILT_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := swresample
-LOCAL_SRC_FILES := libswresample-2.so
-include $(PREBUILT_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := swscale
-LOCAL_SRC_FILES := libswscale-4.so
+LOCAL_MODULE := ffmpeg
+LOCAL_SRC_FILES := libffmpeg.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
@@ -72,7 +37,7 @@ LOCAL_SRC_FILES :=  ./libmp3lame/bitstream.c \
 
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/ffmpeg/include
 
-LOCAL_LDLIBS += -llog
-LOCAL_SHARED_LIBRARIES := avcodec avdevice avfilter avformat avutil postproc swresample swscale
+LOCAL_LDLIBS += -llog -lz
+LOCAL_SHARED_LIBRARIES := ffmpeg
 
 include $(BUILD_SHARED_LIBRARY)
