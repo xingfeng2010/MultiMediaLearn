209e3d1f960b0b07c3667a8c0c39ef95940ecd24
李世星 <70449@any3.com>
2021-11-02 14:37:09 +0800

    更新依赖库版本

--- app/src/main/java/com/player/xingfeng/multimedia/MainActivity.java
+++ app/src/main/java/com/player/xingfeng/multimedia/MainActivity.java
@@ -2,15 +2,15 @@ package com.player.xingfeng.multimedia;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.content.res.AssetManager;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 public class MainActivity extends AppCompatActivity {
 
